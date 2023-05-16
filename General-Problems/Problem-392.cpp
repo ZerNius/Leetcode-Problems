8024a7c2894dd79f81e098aa630feee6c3f702ad
ZerNius <w.udit.shrivastava@gmail.com>
2023-05-16 15:40:52 +0530

    Added Problem-392

---
+++ General-Problems/Problem-392.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0;
+        int j = 0;
+        while(i<s.size() && j<t.size())
+        {
+            if(s[i] == t[j])
+            {
+                i++;
+                j++;
+            }
+            else if(s[i] != t[j])
+            {
+                j++;
+            }
+        }
+        int size = s.size() - 1;
+        if(i > size)
+            return true;
+        else
+            return false;
+    }
+
+};
+
+int main()
+{
+    string s, t;
+    cin>>s>>t;
+
+    Solution sol;
+    cout<<sol.isSubsequence(s, t);
+
+    return 0;
+}
